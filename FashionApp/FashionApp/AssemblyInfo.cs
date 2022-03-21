f3d0a5b8eb54b4cad8420ea7d37fa4be43b550a0
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-03-21 15:51:14 +0100

    worked with font

---
+++ FashionApp/FashionApp/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("PlayfairDisplay-ExtraBold.ttf", Alias ="firstfont")]
+[assembly: ExportFont("PlayfairDisplay-Medium.ttf", Alias ="secondfont")]
+[assembly: ExportFont("PlayfairDisplay-Regular.ttf", Alias ="thirdfont")]
+[assembly: ExportFont("PlayfairDisplay-SemiBold.ttf", Alias ="newfont")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias ="RobotoBold")]
+[assembly: ExportFont("Roboto-LightItalic.ttf", Alias ="RobotoItalic")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias ="RobotoMedium")]
